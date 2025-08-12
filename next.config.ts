d599885c7d591087038ca72c6cf06d1f941a2515
Nirjala Shakya <nirjala.shakya@amniltech.com>
2025-08-12 07:34:35 +0545

    feat(coming-soon): configure comming soon path for now

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/coming-soon',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
