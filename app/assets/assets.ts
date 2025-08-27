ba055392eb6d73b5f6e21b2e88bc4ff9653ef5a0
walla <walla@gmail.com>
2025-08-27 14:50:03 +0545

    small changees

--- app/assets/assets.ts
+++ app/assets/assets.ts
@@ -1,22 +1,16 @@
 import CreativeLogo  from './Creative Hub.png'
 import ItclubLogo from './PrimeLogo.png'
-import developIcon from '../../public/images/develop.svg'
-import innovateIcon from '../../public/images/innovate.svg'
-import collaborateIcon from '../../public/images/collaborate.svg'
-import workshopIcon from '../../public/images/workshop.svg'
-import knowledgeIcon from '../../public/images/PC.svg'
-import mentorIcon from '../../public/images/mentor.svg'
 
 const assets = {
     CreativeLogo,
     ItclubLogo,
     // Using the available icon for all SmallCard components
     // You can replace these with specific icons later
-    developIcon,
-    innovateIcon ,
-    collaborateIcon,
-    workshopIcon,
-    knowledgeIcon,
-    mentorIcon,
+    developIcon: '/images/icon.svg',
+    innovateIcon: '/images/icon.svg',
+    collaborateIcon: '/images/icon.svg',
+    workshopIcon: '/images/icon.svg',
+    knowledgeIcon: '/images/icon.svg',
+    mentorIcon: '/images/icon.svg',
 }
 export default assets
