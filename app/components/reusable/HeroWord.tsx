809e680cb2d0cc7a371e201a20104f890d483a32
derikesh <rikeshsherpa1@gmail.com>
2025-08-28 22:31:35 +0545

    fixed about animation , layouts , and responsive

--- app/components/reusable/HeroWord.tsx
+++ app/components/reusable/HeroWord.tsx
@@ -8,7 +8,11 @@ interface HeroWordProps {
 const HeroWord: React.FC<HeroWordProps> = ({ word }) => (
   <h1 className="flex items-center justify-center font-extrabold text-[40px] md:text-[5vw] sm:text-[6vw] xl:text-[4.5vw] 2xl:text-[72px]">
     <span className="flex items-baseline shiny-text">
-     {word}
+      <ShinyText
+      text={word}
+      disabled={false}
+      />
+     {/* {word} */}
       <span className="inline-block text-[12px] md:text-[14px] xl:text-[16px] text-[#0797A0] ml-1">■</span>
     </span>
   </h1>
