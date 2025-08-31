4f9eb92b4c4afe046d2a3524767bd53ed4d86ae9
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2025-08-31 22:20:53 +0545

    team section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import HeroSection from './components/global/heroSection';
-import About from './components/global/about';
-import History from './components/global/history';
-
+import HeroSection from "./components/global/heroSection";
+import About from "./components/global/about";
+import History from "./components/global/history";
+import Team from "./components/global/teamSection";
 export default function Page() {
   return (
     <>
       <HeroSection />
       <About />
-      <History/>
+      <History />
+      <Team />
     </>
-
   );
 }
