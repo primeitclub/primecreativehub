a398264c57dad43cadfc93a08a8f1abf7fca6117
=Manjil <aryalmanzeel1@gmail.com>
2025-09-07 18:42:43 +0545

    fix : fixing new footer section

--- app/components/global/downfooter.tsx
+++ app/components/global/downfooter.tsx
@@ -1,45 +1,38 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import copyright from '../../../public/images/copyRight.svg'
 import SocialIcons from '../reusable/soical-icons'
 
-
-
 export default function Downfooter() {
-  
   return (
-    <div className='text-center  flex flex-col items-center '>
-      
+    <div className="text-center flex flex-col relative items-center">
+      {/* <div className='w-full h-full absolute bg-[linear-gradient(180deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)]  z-10  '></div> */}
       <div className="text-white">
         <SocialIcons />
-      </div> 
-      <div className=' w-full h-full relative after:content-[" "] after:absolute after:w-full after:bottom-0 after:left-0 after:h-1/3 after:bg-[linear-gradient(90deg,#295D60_1.43%,,#0000_99.78%)]  after:z-[9] '>
-      <h1 className="text-[45px] z-0 relative sm:text-[60px] md:text-[95px] lg:text-[180px] leading-none font-extrabold bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)] flex-grow-1 bg-clip-text text-transparent 
-      ">
-      CREATIVE HUB
-      </h1>
-      
-      </div>
-      
 
+        {/* Wrapper for text + fade overlay */}
+        <div className=" flex flex-col w-full items-center overflow-hidden">
+          <h1
+            className="whitespace-nowrap text-[45px] sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[180px] 
+                       leading-none font-extrabold 
+                       bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)] 
+                       bg-clip-text text-transparent relative z-9"
+          >
+            CREATIVE HUB
+          </h1>
 
-      
-      
-
-
-
-      
-      <div className='flex flex-row items-center justify-center'>
-      <div className='flex relative w-[12px] h-[12px] flex-row bg-black '>
-          <Image src='/images/copyRight.svg'
-                alt="Copyright" fill className='object-contain' />
-          
+          {/* Fade effect only at bottom 10–15% */}
+          <div className="absolute bottom-10 left-0 w-full h-[45%] bg-gradient-to-b from-transparent to-black/100 z-20"></div>
         </div>
-        <div className='text-[16px] p-2 text-center z-index-[10]'><p className='text-[12px] '>2025 Creative Hub. All rights reserved  </p></div>
       </div>
-      
-    </div>
 
+      {/* Footer copyright */}
+      <div className="flex flex-row items-center justify-center w-full h-full bg-black mt-4">
+       
+        <div className=" p-2 text-center z-10 bg-black">
+          <p className='text-[16px] sm:text-[18px] md:text-[20px] lg:text-[24px] xl:text-[24px]  '>© 2025 Creative Hub. All rights reserved</p>
+        </div>
+      </div>
+    </div>
   )
 }
