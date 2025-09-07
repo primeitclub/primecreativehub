6a61bfa60a934a90abb4782859ba26ef693dc335
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2025-09-07 21:21:27 +0545

    updateed responsive

--- app/components/global/teamSection.tsx
+++ app/components/global/teamSection.tsx
@@ -28,7 +28,7 @@ export default function TeamSection() {
       <div
         className="
     relative mx-auto w-full
-    px-[8vw] md:px-[12vw]
+    px-[8vw] md:px-[12vw] lg:px-[15vw]
   "
       >
         <div
@@ -40,7 +40,7 @@ export default function TeamSection() {
             md:h-[520px] md:w-[820px] md:blur-[240px]
             lg:h-[620px] lg:w-[1080px] lg:blur-[280px]
             xl:min-w-[500px] sm:min-w[126px]
-            bg-[#146B61]/50 rounded-full
+            bg-[#146B61]/80 rounded-full
           "
         />
 
@@ -53,7 +53,7 @@ export default function TeamSection() {
         <div
           className="
             pt-12 sm:pt-12 md:pt-18
-            grid gap-5 sm:gap-6 md:gap-5 lg:gap-8 xl:gap-10
+            grid gap-5 sm:gap-6 md:gap-5 lg:gap-6 xl:gap-10
             grid-cols-2 md:grid-cols-3
           "
         >
@@ -65,7 +65,7 @@ export default function TeamSection() {
                 rounded-[8px] bg-[#1C2727] text-white text-center
                 shadow-md ring-1 ring-white/5 border border-[#0797A0]
                 py-4 sm:p-6 md:p-7 lg:p-8
-                min-h-[156px] sm:min-h-[300px] md:min-h-[236px] lg:min-h-[420px] xl:min-h-[506px] 2xl:min-h-[506px]
+                min-h-[156px] sm:min-h-[300px] md:min-h-[236px] lg:min-h-[320px] xl:min-h-[506px] 2xl:min-h-[506px]
                 transition-all duration-300 ease-out
                 hover:-translate-y-2 hover:scale-105 hover:shadow-lg
               "
@@ -76,7 +76,7 @@ export default function TeamSection() {
                     h-24 w-24
                     sm:h-24 sm:w-24
                     md:h-25 md:w-25
-                    lg:h-40 lg:w-40
+                    lg:h-35 lg:w-35
                     xl:h-56 xl:w-56
                     2xl:h-56 2xl:w-56
                     overflow-hidden rounded-full ring-2 ring-white/20
@@ -95,15 +95,15 @@ export default function TeamSection() {
               </div>
 
               <div className="flex flex-1 flex-col items-center">
-                <h3 className=" text-xs sm:text-xs md:text-base lg:text-3xl xl:text-3xl 2xl:text-3xl">
+                <h3 className=" text-xs sm:text-xs md:text-base lg:text-xl xl:text-3xl 2xl:text-3xl">
                   {m.name}
                 </h3>
 
-                <p className="mt-1 text-xs sm:text-lg md:text-xs lg:text-2xl xl:text-2xl 2xl:text-2xl text-white/90">
+                <p className="mt-1 text-xs sm:text-lg md:text-xs lg:text-base xl:text-2xl 2xl:text-2xl text-white/90">
                   {m.role}
                 </p>
 
-                <p className="italic mt-0.5 text-[10px] sm:text-[14px] md:text-[8px] lg:text-base xl:text-base 2xl:text-base text-white/70">
+                <p className="italic mt-0.5 text-[10px] sm:text-[14px] md:text-[8px] lg:text-xs xl:text-base 2xl:text-base text-white/70">
                   {m.subRole}
                 </p>
 
@@ -115,7 +115,7 @@ export default function TeamSection() {
                     className="text-[#0797A0] transition-transform hover:-translate-y-1"
                     aria-label={`${m.name} on Instagram`}
                   >
-                    <BiLogoInstagramAlt className="h-6 w-6 sm:h-9 sm:w-9 md:h-6 md:w-6 lg:w-10 lg:h-10" />
+                    <BiLogoInstagramAlt className="h-6 w-6 sm:h-9 sm:w-9 md:h-6 md:w-6 lg:w-8 lg:h-8" />
                   </a>
                   <a
                     href={m.socials.github ?? "#"}
@@ -124,7 +124,7 @@ export default function TeamSection() {
                     className="text-[#0797A0] transition-transform hover:-translate-y-1"
                     aria-label={`${m.name} on GitHub`}
                   >
-                    <FaGithub className="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6 lg:w-10 lg:h-10" />
+                    <FaGithub className="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6 lg:w-8 lg:h-8" />
                   </a>
                   <a
                     href={m.socials.linkedin ?? "#"}
@@ -133,7 +133,7 @@ export default function TeamSection() {
                     className="text-[#0797A0] transition-transform hover:-translate-y-1"
                     aria-label={`${m.name} on LinkedIn`}
                   >
-                    <FaLinkedin className="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6 lg:w-10 lg:h-10" />
+                    <FaLinkedin className="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6 lg:w-8 lg:h-8" />
                   </a>
                 </div>
               </div>
