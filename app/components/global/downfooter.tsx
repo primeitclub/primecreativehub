bc6b1af5470b19b6c9feb3160b5f08eb9eed66c4
=Manjil <aryalmanzeel1@gmail.com>
2025-09-14 20:38:34 +0545

    fix: fixing  footer section

--- app/components/global/downfooter.tsx
+++ app/components/global/downfooter.tsx
@@ -13,7 +13,7 @@ export default function Downfooter() {
         {/* Wrapper for text + fade overlay */}
         <div className=" flex flex-col w-full items-center overflow-hidden">
           <h1
-            className="whitespace-nowrap text-[45px] sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[180px] 
+            className="whitespace-nowrap text-[45px] sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[175px] 
                        leading-none font-extrabold 
                        bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)] 
                        bg-clip-text text-transparent relative z-9"
@@ -29,8 +29,8 @@ export default function Downfooter() {
       {/* Footer copyright */}
       <div className="flex flex-row items-center justify-center w-full h-full bg-black mt-4">
        
-        <div className=" p-2 text-center z-10 bg-black">
-          <p className='text-[16px] sm:text-[18px] md:text-[20px] lg:text-[24px] xl:text-[24px]  '>© 2025 Creative Hub. All rights reserved</p>
+        <div className=" p-1 text-center z-10 bg-black">
+          <p className='text-[16px] sm:text-[16px] md:text-[16px] lg:text-[16px] xl:text-[16px]  '>© 2025 Creative Hub. All rights reserved</p>
         </div>
       </div>
     </div>
