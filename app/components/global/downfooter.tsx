a8d5714a1f2227f933db7b10038948d78f78c8cd
=Manjil <aryalmanzeel1@gmail.com>
2025-09-16 11:04:26 +0545

    fix : footer responsive

--- app/components/global/downfooter.tsx
+++ app/components/global/downfooter.tsx
@@ -5,33 +5,40 @@ import SocialIcons from '../reusable/soical-icons'
 
 export default function Downfooter() {
   return (
-    <div className="text-center flex flex-col relative items-center">
+    <div className="text-center gap-0 flex w-full flex-col relative items-center">
       {/* <div className='w-full h-full absolute bg-[linear-gradient(180deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)]  z-10  '></div> */}
       <div className="text-white">
         <SocialIcons />
 
         {/* Wrapper for text + fade overlay */}
-        <div className=" flex flex-col w-full items-center overflow-hidden">
+        <div className=" flex flex-col relative w-full items-center ">
+        {/* sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[175px] */}
           <h1
-            className="whitespace-nowrap text-[45px] sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[175px] 
-                       leading-none font-extrabold 
-                       bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_99.78%)] 
+            className="whitespace-nowrap  
+                       leading-none font-extrabold text-[12vw]
+                       bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.61%,#295D60_95.78%)] 
                        bg-clip-text text-transparent relative z-9"
           >
             CREATIVE HUB
           </h1>
 
           {/* Fade effect only at bottom 10–15% */}
-          <div className="absolute bottom-10 left-0 w-full h-[45%] bg-gradient-to-b from-transparent to-black/100 z-20"></div>
+          {/* <div className="absolute bottom-0 left-0 w-full  h-[55%] bg-gradient-to-b from-black/0 to-black/100 z-20"></div> */}
+          {/* <div className="absolute bottom-0 left-0 w-full  h-[55%]  bg-[linear-gradient(180deg,transparent_0.%,rgba(0,0,0,0.5)_70.61%,rgba(0,0,0,1)_95.78%)] z-20"></div> */}
+          <div className="absolute bottom-0 left-0 w-full h-[55%] bg-[linear-gradient(180deg,transparent_0%,rgba(0,0,0,0.5)_40%,rgba(0,0,0,1)_100%)] z-20"></div>
+
         </div>
       </div>
 
       {/* Footer copyright */}
-      <div className="flex flex-row items-center justify-center w-full h-full bg-black mt-4">
-       
-        <div className=" p-1 text-center z-10 bg-black">
-          <p className='text-[16px] sm:text-[16px] md:text-[16px] lg:text-[16px] xl:text-[16px]  '>© 2025 Creative Hub. All rights reserved</p>
+      <div className="flex flex-row items-center relative justify-center w-full h-full ">
+      
+        <div className=" p-1 text-center justify-center flex relative w-full z-10 bg-black">
+        <div className="absolute top-0 left-0 w-full h-[1px] 
+              bg-[linear-gradient(91deg,#041213_20.35%,#23C6C6_50%,#041213_80.65%)] "></div>
+          <p className='text-[16px] sm:text-[16px] md:text-[16px] lg:text-[16px] xl:text-[16px]  '>©   2025 Creative Hub. All rights reserved</p>
         </div>
+        
       </div>
     </div>
   )
