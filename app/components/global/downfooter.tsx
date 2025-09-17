d8a9c7decc1cff48ff736ecbbd4a4643ae890fd3
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2025-09-17 12:10:14 +0545

    updated footer color

--- app/components/global/downfooter.tsx
+++ app/components/global/downfooter.tsx
@@ -11,12 +11,12 @@ export default function Downfooter() {
         <SocialIcons />
 
         {/* Wrapper for text + fade overlay */}
-        <div className=" flex flex-col relative  max-w-[1920px] items-center  ">
+        <div className=" flex flex-col relative items-center  ">
           {/* sm:text-[60px] md:text-[95px] lg:text-[160px] xl:text-[175px] */}
           <h1
             className="whitespace-nowrap  
                        leading-none font-extrabold 
-                       text-[clamp(32px,12vw,175px)] 
+                       text-[clamp(32px,12vw,236px)] 
                        bg-[linear-gradient(90deg,#295D60_3.43%,#23C6C6_50.60%,#295D60_95.78%)] 
                        bg-clip-text text-transparent relative z-1"
           >
@@ -27,13 +27,13 @@ export default function Downfooter() {
           {/* <div className="absolute bottom-0 left-0 w-full  h-[55%] bg-gradient-to-b from-black/0 to-black/100 z-20"></div> */}
           {/* <div className="absolute bottom-0 left-0 w-full  h-[55%]  bg-[linear-gradient(180deg,transparent_0.%,rgba(0,0,0,0.5)_70.61%,rgba(0,0,0,1)_95.78%)] z-20"></div> */}
         </div>
-        <div className="absolute -bottom-2  md:bottom-3 md:blur-[20px] sm:blur-[18px]  lg:blur-[20px]  xl:blur-[27px] sm:-bottom-1 blur-[16px] xs:-bottom-1 left-0 w-full h-[45%] bg-[#020E0F] z-[100] "></div>
+        <div className="absolute -bottom-3  md:bottom-4 md:blur-[20px]  sm:blur-[18px]  lg:blur-[20px]  xl:blur-[27px] sm:-bottom-1 xl:-bottom-3 blur-[16px] xs:-bottom-1 left-0 w-full h-[45%] bg-[#020E0F] z-[100] "></div>
 
         {/* <div className="absolute bottom-0 left-0 w-full h-[55%] bg-[linear-gradient(180deg,transparent_0%,rgba(0,0,0,0.5)_40%,rgba(0,0,0,1)_100%)] z-[100]"></div> */}
       </div>
       {/* Footer copyright */}
       <div className="flex flex-row items-center relative justify-center  w-full h-full z-100">
-        <div className=" xl:p-3 lg:p-3 md:p-2 sm:p-1 text-center justify-center flex relative w-full z-10 bg-black">
+        <div className=" xl:p-3 lg:p-3 md:p-2 sm:p-1 text-center justify-center flex relative w-full z-10 bg-transparent">
           <div
             className="absolute top-0 left-0 w-full h-[1px] 
               bg-[linear-gradient(91deg,#041213_20.35%,#23C6C6_50%,#041213_80.65%)] "
