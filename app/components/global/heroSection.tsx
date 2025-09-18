e1ee34ae32848f71498e583b1458bb3b66177335
=Manjil <aryalmanzeel1@gmail.com>
2025-09-18 17:25:27 +0545

    done by manjil

--- app/components/global/heroSection.tsx
+++ app/components/global/heroSection.tsx
@@ -29,7 +29,7 @@ export default function HeroSection() {
           </div>
 
           <div className="mt-2">
-            <SocialIcons />
+            <SocialIcons  />
           </div>
         </div>
 
